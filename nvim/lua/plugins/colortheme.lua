2fd1fe3592609a6f1b6009cd8e1de9e37cc6ecc4
John Olson <n0atn86@gmail.com>
2025-01-11 16:52:54 -0600

    background changes

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,9 +1,20 @@
 return {
-{ "EdenEast/nightfox.nvim",
-lazy = false,
-priority = 1000,
-config = function()
-	vim.cmd("colorscheme carbonfox")
-	end
-}
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nightfox").setup({
+				options = {
+					transparent = true,
+					styles = {
+						comments = "italic",
+						keywords = "bold",
+						types = "italic,bold",
+					},
+				},
+			})
+			vim.cmd("colorscheme carbonfox")
+		end,
+	},
 }
